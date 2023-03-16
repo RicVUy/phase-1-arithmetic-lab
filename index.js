b4c50bc1e5a4bbf1e7b8f2657909d2641a7f3e87
RicardoUy <rvuy62@yahoo.com>
2023-03-16 10:45:53 -0500

    4th submit

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1*num2;
+multiply;
+let random = Math.floor(Math.random() * 10) +1;
+random;
+const num3 = 11;
+const num4 = 7;
+let mod = num3 % num4;
+mod;
+const num5 = 3;
+const num6 = 20;
+let max = Math.max(num5,num6);
+max;
